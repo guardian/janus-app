707223e248b167ee4f7b97fb053d1e6f066bed26
Kate Whalen <whalenka88@gmail.com>
2020-10-15 15:14:00 +0100

    Add example Statements and tests

---
+++ example/src/test/scala/com/example/StatementsTest.scala
@@ -0,0 +1,55 @@
+package com.example
+
+import com.example.Statements._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class StatementsTest extends AnyFreeSpec with Matchers {
+
+  "policy helper" - {
+    val statements = Seq(
+      s3FullAccess("my-bucket", "/"),
+      s3ReadAccess("your-bucket", "/"),
+      s3ReadAccess("your-bucket", "/")
+    )
+
+    "de-duplicates statements" in {
+      val p = policy(statements :_*)
+      p.statements.distinct shouldBe p.statements
+    }
+
+  }
+
+  "enforceCorrectPath" - {
+    "returns true for /" in {
+      enforceCorrectPath("/") shouldEqual true
+    }
+
+    "returns true for path with leading slash and no trailing slash" in {
+      enforceCorrectPath("/correct/path") shouldEqual true
+    }
+
+    "returns false if no leading slash" in {
+      enforceCorrectPath("invalid/path") shouldEqual false
+    }
+
+    "returns false with trailing and leading slash" in {
+      enforceCorrectPath("/bad/trailing/slash/") shouldEqual false
+    }
+
+    "returns false for edge case of single char" in {
+      enforceCorrectPath("f") shouldEqual false
+    }
+  }
+
+  "hierarchyPath"  - {
+    "builds a '/*' pattern for the path '/'" in {
+      hierarchyPath("/") shouldEqual "/*"
+    }
+
+    "builds a '/my-path/sub-path/*' pattern for the path '/my-path/sub-path'" in {
+      hierarchyPath("/my-path/sub-path") shouldEqual "/my-path/sub-path/*"
+    }
+  }
+}
