6043de4e14c25f7bb49ba33ec82915a6565449a0
Kelvin Chappell <kelvin.chappell@theguardian.com>
2025-01-31 15:59:08 +0000

    Migrate AWS Java SDK from v1 to v2
    In the configTools subproject.
    This project is built as a jar
    and imported into the Janus config project
    and the example project in this repo.

--- configTools/src/main/scala/com/gu/janus/policy/Statements.scala
+++ configTools/src/main/scala/com/gu/janus/policy/Statements.scala
@@ -1,12 +1,18 @@
 package com.gu.janus.policy
 
-import awscala._
-import com.amazonaws.auth.policy.Statement.Effect
+import software.amazon.awssdk.policybuilder.iam.IamEffect.ALLOW
+import software.amazon.awssdk.policybuilder.iam.{
+  IamEffect,
+  IamPolicy,
+  IamStatement
+}
+
+import scala.jdk.CollectionConverters._
 
 object Statements {
 
-  def policy(statements: Seq[Statement]*): Policy = {
-    Policy(statements.flatten.distinct)
+  def policy(statements: Seq[IamStatement]*): IamPolicy = {
+    IamPolicy.builder().statements(statements.flatten.distinct.asJava).build()
   }
 
   private[policy] def enforceCorrectPath(path: String): Boolean = {
@@ -26,41 +32,40 @@ object Statements {
   def s3ReadAccess(
       bucketName: String,
       path: String,
-      effect: Effect = Effect.Allow
-  ): Seq[Statement] = {
+      effect: IamEffect = ALLOW
+  ): Seq[IamStatement] = {
     assert(
       enforceCorrectPath(path),
       s"Provided path should include leading slash and omit trailing slash ($bucketName :: $path)"
     )
     s3ConsoleEssentials(bucketName) :+
-      Statement(
-        effect,
-        Seq(Action("s3:Get*"), Action("s3:List*")),
-        Seq(
-          Resource(s"arn:aws:s3:::$bucketName$path"),
-          Resource(s"arn:aws:s3:::$bucketName${hierarchyPath(path)}")
-        )
-      )
+      IamStatement
+        .builder()
+        .effect(effect)
+        .addAction("s3:Get*")
+        .addAction("s3:List*")
+        .addResource(s"arn:aws:s3:::$bucketName$path")
+        .addResource(s"arn:aws:s3:::$bucketName${hierarchyPath(path)}")
+        .build()
   }
 
   /** Grants full access to a given path in an s3 bucket.
     *
     * Provided path should include leading slash and omit trailing slash.
     */
-  def s3FullAccess(bucketName: String, path: String): Seq[Statement] = {
+  def s3FullAccess(bucketName: String, path: String): Seq[IamStatement] = {
     assert(
       enforceCorrectPath(path),
       s"Provided path should include leading slash and omit trailing slash ($bucketName :: $path)"
     )
     s3ConsoleEssentials(bucketName) :+
-      Statement(
-        Effect.Allow,
-        Seq(Action("s3:*")),
-        Seq(
-          Resource(s"arn:aws:s3:::$bucketName$path"),
-          Resource(s"arn:aws:s3:::$bucketName${hierarchyPath(path)}")
-        )
-      )
+      IamStatement
+        .builder()
+        .effect(ALLOW)
+        .addAction("s3:*")
+        .addResource(s"arn:aws:s3:::$bucketName$path")
+        .addResource(s"arn:aws:s3:::$bucketName${hierarchyPath(path)}")
+        .build()
   }
 
   /** Grants permissions for basic s3 console access (list buckets and
@@ -68,42 +73,42 @@ object Statements {
     *
     * Typically bundled as part of other S3 permissions.
     */
-  def s3ConsoleEssentials(bucketName: String): Seq[Statement] = {
+  def s3ConsoleEssentials(bucketName: String): Seq[IamStatement] = {
     Seq(
-      Statement(
-        Effect.Allow,
-        Seq(
-          Action("s3:ListAllMyBuckets"),
-          Action("s3:GetBucketLocation")
-        ),
-        Seq(Resource("arn:aws:s3:::*"))
-      ),
-      Statement(
-        Effect.Allow,
-        Seq(Action("s3:ListBucket")),
-        Seq(Resource(s"arn:aws:s3:::$bucketName"))
-      )
+      IamStatement
+        .builder()
+        .effect(ALLOW)
+        .addAction("s3:ListAllMyBuckets")
+        .addAction("s3:GetBucketLocation")
+        .addResource("arn:aws:s3:::*")
+        .build(),
+      IamStatement
+        .builder()
+        .effect(ALLOW)
+        .addAction("s3:ListBucket")
+        .addResource(s"arn:aws:s3:::$bucketName")
+        .build()
     )
   }
 
   /** Policy statements that allow basic access to s3 bucket (console view and
     * object list)
     */
-  def s3BucketBasicAccessStatements(bucketName: String): Seq[Statement] = {
+  def s3BucketBasicAccessStatements(bucketName: String): Seq[IamStatement] = {
     Seq(
-      Statement(
-        Effect.Allow,
-        Seq(
-          Action("s3:ListAllMyBuckets"),
-          Action("s3:GetBucketLocation")
-        ),
-        Seq(Resource("arn:aws:s3:::*"))
-      ),
-      Statement(
-        Effect.Allow,
-        Seq(Action("s3:ListBucket")),
-        Seq(Resource(s"arn:aws:s3:::$bucketName"))
-      )
+      IamStatement
+        .builder()
+        .effect(ALLOW)
+        .addAction("s3:ListAllMyBuckets")
+        .addAction("s3:GetBucketLocation")
+        .addResource("arn:aws:s3:::*")
+        .build(),
+      IamStatement
+        .builder()
+        .effect(ALLOW)
+        .addAction("s3:ListBucket")
+        .addResource(s"arn:aws:s3:::$bucketName")
+        .build()
     )
   }
 }
