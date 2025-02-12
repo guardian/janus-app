05ea600073644434fcd5e0d10b529feea007c33d
Kelvin Chappell <kelvin.chappell@theguardian.com>
2025-02-12 10:36:51 +0000

    Inline test fixture

--- test/logic/ViewHelpersTest.scala
+++ test/logic/ViewHelpersTest.scala
@@ -10,27 +10,16 @@ import java.time.Instant
 
 class ViewHelpersTest extends AnyFreeSpec with Matchers {
 
-  private def mkCredentials(
-      accessKeyId: String,
-      secretAccessKey: String,
-      sessionToken: String
-  ) =
-    Credentials
-      .builder()
-      .accessKeyId(accessKeyId)
-      .secretAccessKey(secretAccessKey)
-      .sessionToken(sessionToken)
-      .expiration(Instant.now())
-      .build()
-
   "shellCredentials" - {
     "for a single account" - {
       val creds = List(
-        fooAct -> mkCredentials(
-          "foo-key",
-          "foo-secret",
-          "foo-token"
-        )
+        fooAct -> Credentials
+          .builder()
+          .accessKeyId("foo-key")
+          .secretAccessKey("foo-secret")
+          .sessionToken("foo-token")
+          .expiration(Instant.now())
+          .build()
       )
 
       "includes provided key" in {
@@ -65,16 +54,20 @@ class ViewHelpersTest extends AnyFreeSpec with Matchers {
 
     "for multiple accounts" - {
       val multiCreds = List(
-        fooAct -> mkCredentials(
-          "foo-key",
-          "foo-secret",
-          "foo-token"
-        ),
-        barAct -> mkCredentials(
-          "bar-key",
-          "bar-secret",
-          "bar-token"
-        )
+        fooAct -> Credentials
+          .builder()
+          .accessKeyId("foo-key")
+          .secretAccessKey("foo-secret")
+          .sessionToken("foo-token")
+          .expiration(Instant.now())
+          .build(),
+        barAct -> Credentials
+          .builder()
+          .accessKeyId("bar-key")
+          .secretAccessKey("bar-secret")
+          .sessionToken("bar-token")
+          .expiration(Instant.now())
+          .build()
       )
 
       "puts leading space on all commands to exclude from bash history" in {
