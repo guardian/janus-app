2ceb945fbdf6523f90a7d8f940268e2483736894
TJ Silver <thalia.silver@guardian.co.uk>
2025-02-17 12:30:16 +0000

    fix: format file

--- project/RunClientHook.scala
+++ project/RunClientHook.scala
@@ -4,8 +4,8 @@ import sbt._
 import scala.sys.process.Process
 
 /** Frontend build play run hook.
- * https://www.playframework.com/documentation/2.8.x/SBTCookbook
- */
+  * https://www.playframework.com/documentation/2.8.x/SBTCookbook
+  */
 object RunClientHook {
   def apply(base: File): PlayRunHook = {
     object ClientBuildHook extends PlayRunHook {
@@ -14,19 +14,17 @@ object RunClientHook {
       val install = "npm install"
       val run = "npm run start"
 
-      /**
-       * Executed before play run.
-       *
-       * Run npm install if the node modules directory does not exist.
-       */
+      /** Executed before play run.
+        *
+        * Run npm install if the node modules directory does not exist.
+        */
       override def beforeStarted(): Unit = {
         if (!(base / "frontend" / "node_modules").exists())
           Process(install, base / "frontend").!
       }
 
-      /**
-       * Runs npm start script after play run
-       */
+      /** Runs npm start script after play run
+        */
       override def afterStarted(): Unit = {
         process = Some(
           Process(run, base / "frontend").run
@@ -34,7 +32,7 @@ object RunClientHook {
       }
 
       /** Executed after play run stop. Cleanup frontend execution processes.
-       */
+        */
       override def afterStopped(): Unit = {
         process.foreach(p => p.destroy())
         process = None
