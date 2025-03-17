da26439c19e9f559207e018845cd410487d1a841
Kelvin Chappell <kelvin.chappell@theguardian.com>
2025-03-17 14:46:31 +0000

    Avoid web proxies

--- app/aws/Clients.scala
+++ app/aws/Clients.scala
@@ -1,17 +1,24 @@
 package aws
 
 import software.amazon.awssdk.auth.credentials._
+import software.amazon.awssdk.http.apache.{ApacheHttpClient, ProxyConfiguration}
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.sts.StsClient
 
 import java.net.URI
+import scala.jdk.CollectionConverters._
 
 object Clients {
   // local dev is in a separate profile name so it isn't overwritten when you obtain credentials using Janus
   // this profile needs credentials sufficient to power Janus (specifically assumeRole), which is probably
   // different to the credentials you normally want to use
   val profileName = "janus"
+
+  val proxyConfig =
+    ProxyConfiguration.builder().nonProxyHosts(Set("*").asJava).build()
+  val httpClient =
+    ApacheHttpClient.builder().proxyConfiguration(proxyConfig).build()
 
   private lazy val credentialsProviderChain: AwsCredentialsProviderChain =
     AwsCredentialsProviderChain
@@ -23,6 +30,7 @@ object Clients {
   lazy val stsClient: StsClient =
     StsClient
       .builder()
+      .httpClient(httpClient)
       .credentialsProvider(credentialsProviderChain)
       .region(EU_WEST_1)
       .build()
@@ -30,6 +38,7 @@ object Clients {
   def localDb: DynamoDbClient =
     DynamoDbClient
       .builder()
+      .httpClient(httpClient)
       .credentialsProvider(
         StaticCredentialsProvider.create(
           AwsBasicCredentials.create("fakeMyKeyId", "fakeSecretAccessKey")
