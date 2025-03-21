077c86c35fc1ac4db0044471ee726142ceda3c9a
adamnfish <adam.fisher@guardian.co.uk>
2025-03-21 13:55:02 +0000

    Update the accounts information page
    This adds the account number to the accounts page, which is a critical
    piece of information for properly understanding which account is
    which.
    
    This version of the page pulls the account number from the configured
    role, which is a nice simple way to go about it. A more complete
    solution would use the configured role to call `STS.getCallerIdentity`
    but this isn't currently required and would make this all more
    complicated.
    
    We also tidy up and simplify the "owners" display. The previous
    implementation dates back to Janus' early versions, where there were
    only "devs" and "admins". This assumption no longer holds, so we
    instead list everyone that has any access, and tell users to look at
    the Janus access config to find out more detail.
    
    Here again, a future version could show the JSON policy(s) available
    to each listed user, but since this duplicates the information in
    Janus' configuration there's no need to add this until it is asked
    for.

--- app/controllers/Utility.scala
+++ app/controllers/Utility.scala
@@ -2,16 +2,21 @@ package controllers
 
 import com.gu.googleauth.AuthAction
 import com.gu.janus.model.JanusData
+import conf.Config
 import logic.Owners
-import play.api.Mode
+import play.api.{Configuration, Logging, Mode}
 import play.api.mvc._
+
+import scala.util.{Failure, Try}
 
 class Utility(
     janusData: JanusData,
     controllerComponents: ControllerComponents,
-    authAction: AuthAction[AnyContent]
+    authAction: AuthAction[AnyContent],
+    configuration: Configuration
 )(implicit mode: Mode, assetsFinder: AssetsFinder)
-    extends AbstractController(controllerComponents) {
+    extends AbstractController(controllerComponents)
+    with Logging {
 
   def healthcheck = Action {
     Ok("ok")
@@ -19,9 +24,22 @@ class Utility(
 
   def accounts = authAction { implicit request =>
     val sortedAccounts = janusData.accounts.toList.sortBy(_.name.toLowerCase)
-    val owners = sortedAccounts.map(account =>
-      account -> Owners.accountOwners(account, janusData.access)
-    )
-    Ok(views.html.accounts(owners, request.user, janusData))
+    val accountData =
+      sortedAccounts.map { awsAccount =>
+        (
+          awsAccount,
+          Owners.accountPermissions(awsAccount, janusData.access),
+          Config.accountNumber(awsAccount.authConfigKey, configuration)
+        )
+      }
+    // log any account number errors we accumulated
+    accountData
+      .collect { case (account, _, Failure(err)) =>
+        (account, err)
+      }
+      .foreach { case (account, err) =>
+        logger.warn(s"Couldn't lookup account number for ${account.name}", err)
+      }
+    Ok(views.html.accounts(accountData, request.user, janusData))
   }
 }
