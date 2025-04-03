1f0e7edcaf58ee14048b9e1a39e4689cd3917cb7
TJ Silver <thalia.silver@guardian.co.uk>
2025-04-03 16:52:49 +0100

    feat: trigger passkey flow on click of button on account page

--- frontend/passkeys.js
+++ frontend/passkeys.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    "use strict";
+    'use strict';
 
     async function registerPasskey() {
-        console.log("1");
-        const response = await fetch("/passkey/registration-options") //fetch PublicKeyCredentialCreationOptions as JSON string
+        const response = await fetch('/passkey/registration-options') //fetch PublicKeyCredentialCreationOptions as JSON string
         const publicKeyCredentialCreationOptionsJSON = await response.json() // convert to JSONObject
         const credentialCreationOptions = PublicKeyCredential.parseCreationOptionsFromJSON(publicKeyCredentialCreationOptionsJSON); // convert to PublicKeyCredentialCreationOptions
         const publicKeyCredential = await navigator.credentials.create({publicKey: credentialCreationOptions}); // create PublicKeyCredential
-        console.log("publicKeyCredential: ", publicKeyCredential);
+        console.log('publicKeyCredential: ', publicKeyCredential);
         const registrationResponseJSON = publicKeyCredential.toJSON(); // convert to JSONObject
-        await fetch("/passkey/register", {
+        await fetch('/passkey/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'text/plain',
@@ -19,8 +18,15 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    document.querySelectorAll('register-passkey').forEach(function(el){
-        el.addEventListener('click', function(){console.log('button pressed')});
+    document.querySelectorAll('#register-passkey').forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            registerPasskey().then(function() {
+                console.log('Registration successful');
+            }).catch(function(err) {
+                console.error('Error during registration:', err);
+            });
+        });
     })
 
 });
