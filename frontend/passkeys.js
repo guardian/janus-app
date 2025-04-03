140d798ad766411ead060adcccb6e2a851936634
TJ Silver <thalia.silver@guardian.co.uk>
2025-04-03 14:54:51 +0100

    Co-authored-by: Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>

--- frontend/passkeys.js
+++ frontend/passkeys.js
@@ -1,18 +1,26 @@
-console.log("1");
-(async () => {
-    console.log("2");
-    const response = await fetch("/passkey/registration-options") //fetch PublicKeyCredentialCreationOptions as JSON string
-    const publicKeyCredentialCreationOptionsJSON = await response.json() // convert to JSONObject
-    const credentialCreationOptions = PublicKeyCredential.parseCreationOptionsFromJSON(publicKeyCredentialCreationOptionsJSON); // convert to PublicKeyCredentialCreationOptions
-    const publicKeyCredential = await navigator.credentials.create({publicKey: credentialCreationOptions}); // create PublicKeyCredential
-    console.log("publicKeyCredential: ", publicKeyCredential);
-    const registrationResponseJSON = publicKeyCredential.toJSON(); // convert to JSONObject
-    await fetch("/passkey/register", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain',
-            'Csrf-Token': '@{CSRF.getToken.get.value}'
-        },
-        body: JSON.stringify(registrationResponseJSON)
-    });
-})();
+document.addEventListener('DOMContentLoaded', function() {
+    "use strict";
+
+    async function registerPasskey() {
+        console.log("1");
+        const response = await fetch("/passkey/registration-options") //fetch PublicKeyCredentialCreationOptions as JSON string
+        const publicKeyCredentialCreationOptionsJSON = await response.json() // convert to JSONObject
+        const credentialCreationOptions = PublicKeyCredential.parseCreationOptionsFromJSON(publicKeyCredentialCreationOptionsJSON); // convert to PublicKeyCredentialCreationOptions
+        const publicKeyCredential = await navigator.credentials.create({publicKey: credentialCreationOptions}); // create PublicKeyCredential
+        console.log("publicKeyCredential: ", publicKeyCredential);
+        const registrationResponseJSON = publicKeyCredential.toJSON(); // convert to JSONObject
+        await fetch("/passkey/register", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Csrf-Token': '@{CSRF.getToken.get.value}'
+            },
+            body: JSON.stringify(registrationResponseJSON)
+        });
+    }
+
+    document.querySelectorAll('register-passkey').forEach(function(el){
+        el.addEventListener('click', function(){console.log('button pressed')});
+    })
+
+});
