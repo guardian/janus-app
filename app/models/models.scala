6166ef5351a77f2058ab9b1f487d02a3b130070e
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-04-03 09:42:02 +0100

    feat: add custom exception classes for error handling

--- app/models/models.scala
+++ app/models/models.scala
@@ -1,5 +1,7 @@
 package models
 
+import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR}
+
 sealed trait AccountConfigStatus
 case class FederationConfigError(causedBy: Throwable)
     extends AccountConfigStatus
@@ -11,3 +13,37 @@ sealed trait DisplayMode
 object Normal extends DisplayMode
 object Spooky extends DisplayMode
 object Festive extends DisplayMode
+
+sealed trait JanusException extends Exception {
+  def userMessage: String
+  def engineerMessage: String
+  def httpCode: Int
+}
+sealed trait JanusExceptionWithCause extends JanusException {
+  def cause: Throwable
+}
+case class BadArgumentException(
+    userMessage: String,
+    engineerMessage: String,
+    cause: Throwable
+) extends JanusExceptionWithCause {
+  val httpCode: Int = BAD_REQUEST
+}
+case class AwsCallException(
+    userMessage: String,
+    engineerMessage: String,
+    cause: Throwable
+) extends JanusExceptionWithCause {
+  val httpCode: Int = INTERNAL_SERVER_ERROR
+}
+case class PasskeyVerificationException(
+    userMessage: String,
+    engineerMessage: String,
+    cause: Throwable
+) extends JanusExceptionWithCause {
+  val httpCode: Int = BAD_REQUEST
+}
+case class NotFoundException(userMessage: String, engineerMessage: String)
+    extends JanusException {
+  val httpCode: Int = BAD_REQUEST
+}
