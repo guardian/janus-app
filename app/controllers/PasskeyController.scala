bd34efa508c4bd91457c886bc2d7e871c45692dd
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-04-07 13:47:28 +0100

    refactor: improve error handling in PasskeyController
    To make use of implicit json encoding.

--- app/controllers/PasskeyController.scala
+++ app/controllers/PasskeyController.scala
@@ -5,6 +5,7 @@ import aws.{PasskeyChallengeDB, PasskeyDB}
 import com.gu.googleauth.{AuthAction, UserIdentity}
 import com.gu.janus.model.JanusData
 import io.circe.syntax.EncoderOps
+import io.circe.{Encoder, Json}
 import logic.Passkey
 import models.JanusException
 import models.Passkey._
@@ -13,6 +14,7 @@ import play.api.{Logging, Mode}
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 
 import scala.util.control.NonFatal
+import scala.util.{Success, Try}
 
 /** Controller for handling passkey registration and authentication. */
 class PasskeyController(
@@ -30,39 +32,57 @@ class PasskeyController(
     case Mode.Prod => "Janus-Prod"
   }
 
+  private def apiResponse[A](
+      action: => Try[A]
+  )(implicit encoder: Encoder[A]): Result =
+    action match {
+      case Left(err: JanusException) =>
+        logger.error(err.engineerMessage, err.causedBy.orNull)
+        val json = Json.obj(
+          "status" -> Json.fromString("error"),
+          "message" -> Json.fromString(err.userMessage)
+        )
+        Status(err.httpCode)(json.noSpaces)
+      case NonFatal(err) =>
+        logger.error(err.getMessage, err)
+        val json = Json.obj(
+          "status" -> Json.fromString("error"),
+          "message" -> Json.fromString(err.getClass.getSimpleName)
+        )
+        Status(INTERNAL_SERVER_ERROR)(json.noSpaces)
+      case Success(a) =>
+        Ok(a.asJson.noSpaces)
+    }
+
   /** See
     * [[https://webauthn4j.github.io/webauthn4j/en/#generating-a-webauthn-credential-key-pair]].
     */
   def registrationOptions: Action[AnyContent] = authAction { request =>
-    (for {
-      options <- Passkey.registrationOptions(appName, host, request.user)
-      _ <- PasskeyChallengeDB.insert(
-        UserChallenge(request.user, options.getChallenge)
-      )
-    } yield {
-      logger.info(
-        s"Created registration options for user ${request.user.username}"
-      )
-      Ok(options.asJson.noSpaces)
-    }).fold(handleErrors, identity)
+    apiResponse (
+      for {
+        options <- Passkey.registrationOptions(appName, host, request.user)
+        _ <- PasskeyChallengeDB.insert(
+          UserChallenge(request.user, options.getChallenge)
+        )
+        _ = logger.info(s"Created registration options for user ${request.user.username}")
+      } yield options
+    )
   }
 
   /** See
     * [[https://webauthn4j.github.io/webauthn4j/en/#registering-the-webauthn-public-key-credential-on-the-server]].
     */
   def register: Action[AnyContent] = authAction { request =>
-    (for {
-      challenge <- PasskeyChallengeDB.load(request.user)
-      body <- extractRequestBody(request.body.asText, request.user)
-      credRecord <- Passkey.verifiedRegistration(host, challenge, body)
-      _ <- PasskeyDB.insert(request.user, credRecord)
-      _ <- PasskeyChallengeDB.delete(request.user)
-    } yield {
-      logger.info(
-        s"Registered passkey for user ${request.user.username}"
-      )
-      NoContent
-    }).fold(handleErrors, identity)
+    apiResponse (
+      for {
+        challenge <- PasskeyChallengeDB.load(request.user)
+        body <- extractRequestBody(request.body.asText, request.user)
+        credRecord <- Passkey.verifiedRegistration(host, challenge, body)
+        _ <- PasskeyDB.insert(request.user, credRecord)
+        _ <- PasskeyChallengeDB.delete(request.user)
+        _ = logger.info(s"Registered passkey for user ${request.user.username}")
+      } yield ()
+    )
   }
 
   private def extractRequestBody(body: Option[String], user: UserIdentity) =
@@ -77,14 +97,4 @@ class PasskeyController(
         )
       )
       .toTry
-
-  private def handleErrors(exception: Throwable): Result =
-    exception match {
-      case exception: JanusException =>
-        logger.error(exception.engineerMessage, exception.causedBy.get)
-        Status(exception.httpCode)(exception.userMessage)
-      case NonFatal(exception) =>
-        logger.error(exception.getMessage, exception)
-        Status(INTERNAL_SERVER_ERROR)
-    }
 }
