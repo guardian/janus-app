efb4aeb4fa230b7909f5946479e3d6c9f406cf10
TJ Silver <thalia.silver@guardian.co.uk>
2025-04-15 17:38:47 +0100

    refactor: use passkeys file as a module in janus.js

--- frontend/passkeys.js
+++ frontend/passkeys.js
@@ -1,31 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    'use strict';
+export async function registerPasskey(csrfToken) {
+    const response = await fetch('/passkey/registration-options');
+    const publicKeyCredentialCreationOptionsJSON = await response.json();
+    const credentialCreationOptions = PublicKeyCredential.parseCreationOptionsFromJSON(publicKeyCredentialCreationOptionsJSON);
+    const publicKeyCredential = await navigator.credentials.create({ publicKey: credentialCreationOptions });
+    const registrationResponseJSON = publicKeyCredential.toJSON();
+    await fetch('/passkey/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+            'Csrf-Token': csrfToken
+        },
+        body: JSON.stringify(registrationResponseJSON)
+    });
+}
 
-    const registerPasskeyButton = document.querySelector('#register-passkey');
-
-
-    async function registerPasskey(csrfToken) {
-        const response = await fetch('/passkey/registration-options') 
-        const publicKeyCredentialCreationOptionsJSON = await response.json() 
-        const credentialCreationOptions = PublicKeyCredential.parseCreationOptionsFromJSON(publicKeyCredentialCreationOptionsJSON); 
-        const publicKeyCredential = await navigator.credentials.create({publicKey: credentialCreationOptions}); 
-        const registrationResponseJSON = publicKeyCredential.toJSON(); 
-        await fetch('/passkey/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'text/plain',
-                'Csrf-Token': csrfToken
-            },
-            body: JSON.stringify(registrationResponseJSON)
+export function setupRegisterPasskeyButton(buttonSelector) {
+    const registerPasskeyButton = document.querySelector(buttonSelector);
+    registerPasskeyButton?.addEventListener('click', function (e) {
+        e.preventDefault();
+        const csrfToken = this.getAttribute('csrf-token');
+        registerPasskey(csrfToken).catch(function (err) {
+            console.error(err);
         });
-    }
-
-    registerPasskeyButton?.addEventListener('click', function(e) {
-            e.preventDefault();
-            const csrfToken = this.getAttribute('csrf-token');
-            registerPasskey(csrfToken).catch(function(err) {
-                console.error(err);
-            });
-        });
-
-});
+    });
+}
