39f0fef88af4c42a6414b231daa013e5a896fcf3
TJ Silver <thalia.silver@guardian.co.uk>
2025-06-18 18:43:12 +0100

    feat: move displayFlashMessages function into module

--- frontend/passkeys.js
+++ frontend/passkeys.js
@@ -287,31 +287,3 @@ export function setUpProtectedLinks(links) {
         });
     });
 }
-
-/**
- * Displays flash messages from the server as toasts
- * @param {Object} flashMessages Object containing flash messages by type
- */
-export function displayFlashMessages(flashMessages) {
-    if (!flashMessages) { 
-        return 
-    }
-    if (flashMessages.success) {
-        M.toast({
-            html: DOMPurify.sanitize(flashMessages.success),
-            classes: 'green lighten-1 rounded',
-        });
-    }
-    if (flashMessages.info) {
-        M.toast({
-            html: DOMPurify.sanitize(flashMessages.info),
-            classes: 'blue lighten-1 rounded',
-        });
-    }
-    if (flashMessages.error) {
-        M.toast({
-            html: DOMPurify.sanitize(flashMessages.error),
-            classes: 'red lighten-1 rounded',
-        });
-    }
-}
