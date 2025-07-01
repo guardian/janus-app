895788bb7048eb4277725f70322fcb25edac41ce
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-07-01 15:33:03 +0100

    refactor: Remove Jest test scripts and config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,25 +1,8 @@
 import guardian from '@guardian/eslint-config';
 
 export default [
-    ...guardian.configs.recommended,
-    {
+	...guardian.configs.recommended,
+	{
         ignores: ["dist/**"],
-    },
-    {
-        // Add Jest globals
-        files: ["*.test.js"],
-        languageOptions: {
-            globals: {
-                jest: true,
-                describe: true,
-                it: true,
-                test: true,
-                expect: true,
-                beforeEach: true,
-                afterEach: true,
-                beforeAll: true,
-                afterAll: true,
-            }
-        }
     }
 ];
--- frontend/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js'],
-    transformIgnorePatterns: ['/node_modules/']
-};
