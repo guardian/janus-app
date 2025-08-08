a87f5de7b8751dd72803c70a32de23214eb88058
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-08-08 16:47:14 +0100

    feat: Can verify authentication data

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -2,6 +2,7 @@ import aws.{Clients, PasskeyChallengeDB, PasskeyDB}
 import com.gu.googleauth.{AuthAction, UserIdentity}
 import com.gu.play.secretrotation.*
 import com.gu.play.secretrotation.aws.parameterstore
+import com.gu.playpasskeyauth.filters.PasskeyVerificationFilter
 import com.gu.playpasskeyauth.models.HostApp
 import com.gu.playpasskeyauth.services.{PasskeyChallengeRepository, PasskeyRepository, PasskeyVerificationServiceImpl}
 import com.typesafe.config.ConfigException
@@ -114,19 +115,6 @@ class AppComponents(context: ApplicationLoader.Context)
       )
   private val passkeysEnablingCookieName: String =
     configuration.get[String]("passkeys.enablingCookieName")
-
-  private val passkeyAuthFilter =
-    new PasskeyAuthFilter(
-      host,
-      passkeysEnabled,
-      passkeysEnablingCookieName
-    )
-  private val passkeyRegistrationAuthFilter =
-    new PasskeyRegistrationAuthFilter(passkeyAuthFilter)
-
-  private val passkeyAuthAction = authAction.andThen(passkeyAuthFilter)
-  private val passkeyRegistrationAuthAction =
-    authAction.andThen(passkeyRegistrationAuthFilter)
 
   private val pkRepo = new PasskeyRepository {
     override def loadCredentialRecord(
@@ -308,13 +296,31 @@ class AppComponents(context: ApplicationLoader.Context)
       challengeRepo = pkChallRepo
     )
 
+  import controllers.given
+  private val passkeyVerificationFilter =
+    new PasskeyVerificationFilter(
+      pkService
+    )
+  private val passkeyAuthFilter =
+    new PasskeyAuthFilter(
+      host,
+      passkeysEnabled,
+      passkeysEnablingCookieName
+    )
+  private val passkeyRegistrationAuthFilter =
+    new PasskeyRegistrationAuthFilter(passkeyAuthFilter)
+
+  private val passkeyVerificationAction = authAction.andThen(passkeyVerificationFilter)
+  private val passkeyRegistrationAuthAction =
+    authAction.andThen(passkeyRegistrationAuthFilter)
+
   override def router: Router = new Routes(
     httpErrorHandler,
     new Janus(
       janusData,
       controllerComponents,
       authAction,
-      passkeyAuthAction,
+      passkeyVerificationAction,
       host,
       Clients.stsClient,
       configuration,
@@ -324,7 +330,7 @@ class AppComponents(context: ApplicationLoader.Context)
     new PasskeyController(
       controllerComponents,
       authAction,
-      passkeyAuthAction,
+      passkeyVerificationAction,
       passkeyRegistrationAuthAction,
       host,
       janusData,
