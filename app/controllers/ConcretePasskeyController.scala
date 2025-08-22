02f0b7fae3605cfc269714a61f33eb14a310137f
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2025-08-22 16:36:32 +0100

    refactor: Implement ChallengeRepository

--- app/controllers/ConcretePasskeyController.scala
+++ app/controllers/ConcretePasskeyController.scala
@@ -15,36 +15,11 @@ import play.api.mvc.{
 
 import scala.concurrent.ExecutionContext
 
-given RequestExtractor[UserIdentityRequest] with {
-
-  def findUserId[A](request: UserIdentityRequest[A]): Option[String] = Some(
-    request.user.username
-  )
-
-  def findCreationData[A](request: UserIdentityRequest[A]): Option[JsValue] =
-    request.body match {
-      case AnyContentAsFormUrlEncoded(data) =>
-        data.get("passkey").flatMap(_.headOption.map(Json.parse))
-      case _ => None
-    }
-
-  def findAuthenticationData[A](
-      request: UserIdentityRequest[A]
-  ): Option[JsValue] =
-    request.body match {
-      case AnyContentAsFormUrlEncoded(data) =>
-        data.get("credentials").flatMap(_.headOption.map(Json.parse))
-      // Used in authenticate before registration context?
-      case AnyContentAsJson(authData) => Some(authData)
-      case _                          => None
-    }
-}
-
 class ConcretePasskeyController(
     controllerComponents: ControllerComponents,
     customAction: ActionBuilder[UserIdentityRequest, AnyContent],
     passkeyService: PasskeyVerificationService
-)(using ExecutionContext)
+)(using RequestExtractor[UserIdentityRequest], ExecutionContext)
     extends BasePasskeyController(
       controllerComponents,
       customAction,
