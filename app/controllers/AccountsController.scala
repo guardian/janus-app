f4c8dededa25235f87a1bae1d8f77f497a242021
adamnfish-gu <adam.fisher@guardian.co.uk>
2026-01-29 10:15:57 +0000

    Improve loading display and tidy up controller
    It wasn't clear that the "loading" icon meant loading was happening -
    it looked like a refresh buttoin. This is helped by adding rotation.
    
     The fake data in the AccountsController is tidied up a little.

--- app/controllers/AccountsController.scala
+++ app/controllers/AccountsController.scala
@@ -81,42 +81,43 @@ class AccountsController(
 
   def accountRoles: Action[AnyContent] = authAction { implicit request =>
     val realData = provisionedRoleStatusManager.getCacheStatus
-    val fakeData = realData.map { case (account, status) =>
-      val augmentedSnapshot =
-        (status.roleSnapshot, status.failureStatus) match {
-          case (None, Some(failure)) =>
-            Some(
-              IamRoleInfoSnapshot(
-                roles = List.fill(scala.util.Random.between(0, 11))(
-                  IamRoleInfo(
-                    roleArn = Arn.fromString(
-                      s"arn:aws:iam::012345678901:role/gu/janus/discoverable/fake-role"
-                    ),
-                    provisionedRoleTagValue = "fake-provisioned-role",
-                    friendlyName = Some("Fake Role"),
-                    description = Some("This is a fake role for testing."),
-                    account = account
-                  )
-                ),
-                timestamp = java.time.Instant.now()
+    // this fake data augments what gets fetched so we can design with populated accounts
+    val fakeData = realData
+      .map { case (account, status) =>
+        val augmentedSnapshot =
+          (status.roleSnapshot, status.failureStatus) match {
+            case (None, Some(failure)) =>
+              Some(
+                IamRoleInfoSnapshot(
+                  roles = List.fill(scala.util.Random.between(0, 11))(
+                    IamRoleInfo(
+                      roleArn = Arn.fromString(
+                        s"arn:aws:iam::012345678901:role/gu/janus/discoverable/fake-role"
+                      ),
+                      provisionedRoleTagValue = "fake-provisioned-role",
+                      friendlyName = Some("Fake Role"),
+                      description = Some("This is a fake role for testing."),
+                      account = account
+                    )
+                  ),
+                  timestamp = java.time.Instant.now()
+                )
               )
-            )
-          case _ =>
-            status.roleSnapshot
+            case _ =>
+              status.roleSnapshot
 
-        }
-      account -> status.copy(roleSnapshot = augmentedSnapshot)
-    }
-    val accountRolesAndStatus = Accounts.getAccountRolesAndStatus(fakeData)
-    val tmp = fakeData.toList.sortBy(_._1.name)
+          }
+        account -> status.copy(roleSnapshot = augmentedSnapshot)
+      }
+      .toList
+      .sortBy(_._1.name)
     Ok(
       views.html
         .accountRoles(
-          fakeData.toList.sortBy(_._1.name),
+          fakeData,
           request.user,
           janusData
         )
     )
   }
-
 }
